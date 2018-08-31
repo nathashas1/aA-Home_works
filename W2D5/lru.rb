8d6128a86bb170e68799c4eb327f81f009857c76
‘nathashas1’ <nathasha36@gmail.com>
2018-08-30 22:06:15 -0700

    lru cache

---
+++ W2D5/lru.rb
@@ -0,0 +1,34 @@
+class LRUCache
+    def initialize(length)
+      @length = length
+      @cache = []
+    end
+
+    def count
+      # returns number of elements currently in cache
+      @cache.length
+    end
+
+    def add(el)
+      # adds element to cache according to LRU principle
+      if @cache.include?(el)
+         @cache.delete(el)
+         @cache << el
+       elsif self.count >= @size
+         @cache.shift
+         @cache << el
+       else
+         @cache << el
+      end
+    end
+
+    def show
+      # shows the items in the cache, with the LRU item first
+    p @cache
+    nil
+    end
+
+    private
+    # helper methods go here!
+
+  end
