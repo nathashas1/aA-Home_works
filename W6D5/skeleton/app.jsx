5f6fc569ea3ce658d549ef71210b666a5deac50d
‘nathashas1’ <nathasha36@gmail.com>
2018-09-28 08:51:30 -0700

    react calculator

---
+++ W6D5/skeleton/app.jsx
@@ -0,0 +1,11 @@
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Calculator from './calculator';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<Calculator />, document.getElementById('main'));
+});
